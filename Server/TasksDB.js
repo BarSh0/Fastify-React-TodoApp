ea13c70e02fabc84e0bdf0bd974aff08ab83de1a
Bar <barshoshani274@gmail.com>
2022-03-27 16:00:03 +0300

    update

---
+++ Server/TasksDB.js
@@ -0,0 +1,7 @@
+let tasks = [
+   {id: '1' , description: 'Item One', status: false}, 
+   {id: '2' , description: 'Item TWo', status: false}, 
+   {id: '3' , description: 'Item Three', status: false}, 
+]
+
+module.exports = tasks
